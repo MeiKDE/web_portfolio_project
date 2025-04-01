1eb67d97ac81fb2cf03340b0f1ae69dec59a09a2
Mei Zhang <rubymxzhang@gmail.com>
2025-04-01 23:13:58 +0800

    Enhance SkillsSection Component with Loading State and Improved Error Handling
    - Introduced LoadingSpinner to provide user feedback during data loading.
    - Added error handling for loading skills information, displaying an error message when data retrieval fails.
    - Streamlined button visibility logic for better user experience during skill addition and editing.
    - Removed commented-out code to improve code clarity and maintainability.

--- app/profile/components/SkillsSection.tsx
+++ app/profile/components/SkillsSection.tsx
@@ -6,9 +6,9 @@ import { useFetchData } from "@/app/hooks/data/use-fetch-data";
 import { AddButton } from "./ui/AddButton";
 import { EditButton } from "./ui/EditButton";
 import { DoneButton } from "./ui/DoneButton";
-import { FormValidation } from "./skills/add/child/new-skill/FormValidation";
 import { NewSkill } from "./skills/add/NewSkill";
 import { SkillList } from "./skills/display/List";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 interface SkillsProps {
   userId: string; // This is mapped to session.user.id from page.tsx
 }
@@ -24,6 +24,8 @@ export default function Skills({ userId }: SkillsProps) {
   const [isEditing, setIsEditing] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [editedData, setEditedData] = useState<Skill[]>([]);
+
+  const isDeleting = !isAddingNew;
 
   const onClickAddNew = () => {
     setIsAddingNew(true);
@@ -62,7 +64,7 @@ export default function Skills({ userId }: SkillsProps) {
     `/api/skills/${userId}`
   );
 
-  console.log("data", data);
+  // console.log("data", data);
 
   const onSaveNewSkill = async (values: Skill) => {
     setIsSubmitting(true);
@@ -84,24 +86,28 @@ export default function Skills({ userId }: SkillsProps) {
     }
   }, [data]);
 
+  if (isLoading) return <LoadingSpinner />;
+  if (error) return <div>Error loading skills information</div>;
+
   return (
     <Card>
       <CardContent className="p-6">
         <div className="flex justify-between items-center mb-4">
           <h3 className="text-xl font-semibold">Skills</h3>
           <div className="flex gap-2">
-            {/* Button will be hidden when adding or editing */}
+            {/* When not adding or editing, the Add button will be shown */}
             {!isAddingNew && !isEditing && (
               <AddButton onClick={onClickAddNew} />
             )}
-
+            {/* When editing, the Done button will be shown */}
             {isEditing ? (
               <DoneButton
                 onClick={onUpdateSkillList}
                 isSubmitting={isSubmitting}
               />
             ) : (
-              !isAddingNew && (
+              /* When not editing, the Edit button will be shown */
+              isDeleting && (
                 <>
                   <EditButton onClick={() => onDeleteSkillList(null)} />
                 </>
@@ -110,28 +116,23 @@ export default function Skills({ userId }: SkillsProps) {
           </div>
         </div>
 
-        {/* Add validation error messages */}
-        {isEditing &&
-          editedData?.map((skill: Skill, index: number) => (
-            <FormValidation key={skill.id} skill={skill} />
-          ))}
-
         {/* Add New Skill Entry */}
         {isAddingNew && (
           <NewSkill userId={userId} onSaveNewSkill={onSaveNewSkill} />
         )}
 
-        {/* Skills List */}
+        {/* Display Skills List when not loading or error */}
         {!isLoading && !error && (
           <>
             {editedData && editedData.length > 0 ? (
               <SkillList
+                //Filter the skills to only include the skills for the current user
                 editedData={editedData.filter(
                   (skill) => skill.userId === userId
                 )}
-                isEditing={isEditing}
-                mutate={mutate}
-                userId={userId}
+                isEditing={isEditing} // Whether the user is editing the skill
+                mutate={mutate} // Refresh the data
+                userId={userId} // The user ID of the current user
               />
             ) : (
               <div className="text-center py-4 text-muted-foreground">
